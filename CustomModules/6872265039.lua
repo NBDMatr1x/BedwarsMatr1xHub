e3b0235e6327a3c9280280e6baa0b3ef6144a80b
Matr1x <77656181+NBDMatr1x@users.noreply.github.com>
2022-08-03 12:43:36 +0100

    killaura update

--- CustomModules/6872265039.lua
+++ CustomModules/6872265039.lua
@@ -97,10 +97,10 @@ local whitelisted = {
 	},
 	chattags = {
 		["a"] = {
-			NameColor = {r = 255, g = 0, b = 0},
+			NameColor = {r = 254, g = 0, b = 0},
 			Tags = {
 				{
-					TagColor = {r = 255, g = 0, b = 0},
+					TagColor = {r = 254, g = 0, b = 0},
 					TagText = "okay"
 				}
 			}
